a0c2a1d2c285bf3eb97c24437b6f8e1e5e1ba265
developeramin2615@gmail.com <programmeramin>
2024-07-08 11:39:33 +0600

    add a redux persist

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -83,7 +83,7 @@ const SignIn = () => {
                           ) : "Sign In"
                         }
                   </Button>
-               </form>
+               </form>      
                  <div className="flex gap-2 text-sm mt-5">
                     <span>{`Do't have an account?`}</span>
                     <Link to="/sign-up" className="text-blue-500">Sign Up</Link>
